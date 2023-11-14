2ab27e6b795640d46182cb8d5b94e9858c6b65af
Masood <masoodacheampong@gmail.com>
2023-11-14 23:03:19 +0000

    Simple todo app using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import TaskList from "./components/TaskList";
 
 let nextId = 3;
@@ -8,31 +8,53 @@ const initialTasks = [
   { id: 2, text: "Lennon Wall pic", done: false },
 ];
 
+function tasksReducer(tasks, action) {
+  if (action.type === "added") {
+    return [...tasks, { id: action.id, text: action.text, done: false }];
+  } else if (action.type === "changed") {
+    return tasks.map((task) => {
+      if (task.id === action.task.id) {
+        return action.task;
+      } else {
+        return task;
+      }
+    });
+  } else if (action.type === "deleted") {
+    return tasks.filter((task) => task.id !== action.id);
+  } else {
+    throw Error("Unknown action!" + action.type);
+  }
+}
+
 function App() {
   const [text, setText] = useState("");
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+  // Using Reducers describing the user's intent
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
 
-  const handleOnAddTask = (e) => {
-    e.preventDefault();
-  };
-  const handleChangeTask = (task) => {
-    setTasks(
-      tasks.map((checked) => {
-        if (checked.id === task.id) {
-          return task;
-        } else {
-          return checked;
-        }
-      })
-    );
-  };
-  const handleDeleteTask = (id) => {
-    setTasks(tasks.filter((idx) => idx.id !== id));
-  };
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
   return (
     <div>
       <h1>Prague itenerary</h1>
-      <form onSubmit={handleOnAddTask}>
+      <form onSubmit={handleAddTask}>
         <input
           type="text"
           onChange={(e) => setText(e.target.value)}
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TaskList({ tasks, onChangeTask, onDeleteTask, onChange }) {
-  return tasks.map((task) => {
-    return (
-      <div key={task.id}>
-        <input
-          type="checkbox"
-          checked={task.done}
-          onChange={(e) => onChangeTask({ ...task, done: e.target.checked })}
-        />
-        {task.text}
-        <button onClick={() => onDeleteTask(task.id)}>Delete</button>
-      </div>
-    );
-  });
+export default function TaskList({ tasks, onChangeTask, onDeleteTask }) {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <Task task={task} onChange={onChangeTask} onDelete={onDeleteTask} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default TaskList;
+function Task({ task, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input
+          value={task.text}
+          onChange={(e) =>
+            onChange({
+              ...task,
+              text: e.target.value,
+            })
+          }
+        />
+        <button onClick={() => setIsEditing(false)}>Save</button>
+      </>
+    );
+  } else {
+    taskContent = (
+      <>
+        {task.text}
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+      </>
+    );
+  }
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={(e) => {
+          onChange({
+            ...task,
+            done: e.target.value,
+          });
+        }}
+      />
+      {taskContent}
+      <button onClick={() => onDelete(task.id)}>Delete</button>
+    </label>
+  );
+}
