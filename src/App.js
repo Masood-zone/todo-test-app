d60f04a5b67a6b3c79c36403aa282ceb86862a09
Masood <masoodacheampong@gmail.com>
2023-11-14 23:12:13 +0000

    Restructuring the app, adding designs.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,10 @@
-import { useReducer, useState } from "react";
-import TaskList from "./components/TaskList";
-
-let nextId = 3;
-const initialTasks = [
-  { id: 0, text: "Visit Kafka Museum", done: true },
-  { id: 1, text: "Watch a puppet show", done: false },
-  { id: 2, text: "Lennon Wall pic", done: false },
-];
-
-function tasksReducer(tasks, action) {
-  if (action.type === "added") {
-    return [...tasks, { id: action.id, text: action.text, done: false }];
-  } else if (action.type === "changed") {
-    return tasks.map((task) => {
-      if (task.id === action.task.id) {
-        return action.task;
-      } else {
-        return task;
-      }
-    });
-  } else if (action.type === "deleted") {
-    return tasks.filter((task) => task.id !== action.id);
-  } else {
-    throw Error("Unknown action!" + action.type);
-  }
-}
+import React from "react";
+import MainTodoApp from "./Todo-main";
 
 function App() {
-  const [text, setText] = useState("");
-  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-  // Using Reducers describing the user's intent
-  function handleAddTask(text) {
-    dispatch({
-      type: "added",
-      id: nextId++,
-      text: text,
-    });
-  }
-
-  function handleChangeTask(task) {
-    dispatch({
-      type: "changed",
-      task: task,
-    });
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: "deleted",
-      id: taskId,
-    });
-  }
   return (
     <div>
-      <h1>Prague itenerary</h1>
-      <form onSubmit={handleAddTask}>
-        <input
-          type="text"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        />
-        <button type="submit">Add</button>
-      </form>
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
+      <MainTodoApp />
     </div>
   );
 }
